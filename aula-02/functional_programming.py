d63a3c6d39bd38e0944fe59aeac8ec2e6551e97d
imbrito <ismael.20rj@gmail.com>
2019-04-05 04:37:28 -0300

    tests case to functional programming

---
+++ aula-02/functional_programming.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from itertools import permutations
+
+def combinatorial_analysis(value):
+    '''
+    Utilizando os métodos de programação funcional, complete o método combinatorial_analysis e caso de teste, de modo que o mesmo receba como 
+    parâmetro um número inteiro positivo N (0 < N < 1000), e retorne uma lista com as combinações possíveis para os algarismos 
+    que compõem N, observando ainda as seguintes restrições: 
+        ( i ) não devem ser considerados no resultado números, iniciados por 0; 
+        ( ii ) não devem ser geradas combinações repetidas;
+
+        Exemplo: 120 -> [102, 120, 201, 210]
+
+    Para gerar as combinações possíveis, utilize o método permutations do módulo itertools, visite o link:
+    https://docs.python.org/3.7/library/itertools.html#itertools.permutations
+
+    '''
+    pass
---
+++ aula-02/tests/test_functional_programming.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from unittest import TestCase
+from math import pi, factorial
+from functools import reduce
+from functional_programming import combinatorial_analysis
+
+radius = lambda x: x / (2 * pi)
+triangle_area = lambda x, y: (x * y) / 2
+rectangle_area = lambda x, y: x * y
+square_area = lambda x: x ** 2
+impar = lambda x: x % 2
+par = lambda x: not x % 2
+
+class FunctionalProgrammingSpec(TestCase):
+   
+    def test_it_lambda(self):
+        self.assertEqual(1, (lambda x: x ** 2)(1))
+        self.assertEqual(9, (lambda x: x ** 2)(3))
+        self.assertEqual(2.39, round(radius(15), 2))
+        self.assertEqual(3, triangle_area(2,3))
+        self.assertEqual(6, rectangle_area(2,3))
+        self.assertEqual(4, square_area(2))
+    
+    def test_it_filter(self):
+        self.assertEqual([1,3,5,7,9], list(filter(impar, range(10))))
+        self.assertEqual([0,2,4,6,8], list(filter(par, range(10))))
+    
+    def test_it_map(self):
+        self.assertEqual([9,8,7,6,5], list(map(abs, [-9,-8,-7,-6,-5])))
+        self.assertEqual(['0','1','2','3','4'], list(map(str, range(5))))
+        self.assertEqual([1,4,9,16,25,36,49], list(map(square_area, range(1,8))))
+        self.assertEqual([1, 1, 2, 6, 24, 120, 720], list(map(factorial, range(7))))
+
+    def test_it_reduce(self):
+        self.assertEqual(sum(range(10)), reduce(lambda x,y: x + y, range(10)))
+        self.assertEqual(factorial(6), reduce(lambda x,y: x * y, range(1,7), 1))
+
+    def test_it_map_reduce_fake(self):
+        words = {"map": [1,1,1], "reduce":[1,1,1,1,1,1]}
+        reduce_by_key = lambda x: (x, reduce(lambda x, y: x + y, words[x]))
+        self.assertEqual([('map',3),('reduce',6)], sorted(list(map(reduce_by_key, words.keys()))))
+    
+    def test_it_zip(self):
+        self.assertEqual([(0, 1), (2, 3), (4, 5), (6, 7), (8, 9)], list(zip([0,2,4,6,8],[1,3,5,7,9])))
+        self.assertEqual([('l','o'),('v','e')], list(zip(list('lv'),list('oe'))))
+
+    def test_it_list_comprehension(self):
+        self.assertEqual(list(filter(par, range(10))), [x for x in range(10) if not x % 2])
+        self.assertEqual(list(filter(impar, range(10))), [x for x in range(10) if x % 2])
+        self.assertEqual(list(map(square_area, range(1,8))), [pow(x,2) for x in range(1,8)])
+        self.assertEqual(list('bit'), [x for x in 'bit'])
+    
+    def test_it_dict_comprehension(self):
+        self.assertEqual({'A':0,'B':1,'C':2,'D':3}, dict([(chr(65+x), x) for x in range(4)]))
+        self.assertEqual({0:1,1:1,2:2,3:6,4:24,5:120,6:720}, {x : factorial(x) for x in range(7)})
+
+    def test_it_combinatorial_analysis(self):
+        pass
